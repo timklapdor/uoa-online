6357439a59cba571bb1d869d9a2bcdb0f0f58b19
Tim Klapdor <tim.klapdor@gmail.com>
2025-05-06 22:49:56 +0930

    update tina config for clientID

--- tina/config.ts
+++ tina/config.ts
@@ -8,8 +8,8 @@ const branch =
 
 export default defineConfig({
   branch: "main", 
-  clientId: process.env.TINA_CLIENT_ID || "",
-  token: process.env.TINA_TOKEN || "",
+  clientId: "${{ secrets.TINA_CLIENT_ID }}",
+  token: "${{ secrets.TINA_TOKEN }}",
 
   build: {
     outputFolder: "admin",
